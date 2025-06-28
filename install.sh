782b640827fe80c6b354f2c21901fc88a1d0fd4c
dungemeister <yuriy.vazha@gmail.com>
2025-06-28 23:41:11 +0400

    Added readme and install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Installing GL libraries"
+sudo apt install libgl-dev libglfw3-dev libglew-dev libglm-dev libstb-dev
---
+++ readme.md
@@ -0,0 +1,4 @@
+## Необходимые пакеты
+```bash
+    sudo apt install libgl-dev libglfw3-dev libglew-dev libglm-dev libstb-dev
+```
