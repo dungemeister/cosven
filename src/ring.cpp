6266b85352a2d1d4de8afe222fb46d483fc0963e
dungemeister <yuriy.vazha@gmail.com>
2025-10-31 14:40:03 +0400

    Added plane path for sattelite to render part

--- src/ring.cpp
+++ src/ring.cpp
@@ -55,8 +55,8 @@ void Ring::rotateRing(float d_angle){
 
 void Ring::render(GLuint shaderProgram, GLuint segmentShaderProgram, const glm::mat4& view, const glm::mat4& projection, GLuint borderShaderProgram){
     m_satellites_model->Render(shaderProgram, view, projection, borderShaderProgram);
-    // if(m_segments)
-        // m_segments->Render(segmentShaderProgram, projection * view);
+    if(m_segments)
+        m_segments->Render(segmentShaderProgram, projection * view);
 }
 
 glm::mat4 Ring::MoveToPosition(const glm::mat4& currentTransform, const glm::vec3& target) {
